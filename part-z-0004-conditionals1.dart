b28ced040acbe6e7de50cbd81ab18ed4687efab9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-30 09:35:39 +0300

    Create part-z-0004-conditionals1.dart

---
+++ part-z-0004-conditionals1.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  var x = 5;
+  var y = 3.13;
+  var z = 7;
+
+  if (x > y && y == z) {
+    print("Way 1");
+  } else if (y != z || 2 * z + 1 < 10) {
+    print("Way 2");
+  } else {
+    print("Way 3");
+  }
+}
+
+// Way 2
