a5f272430e014f80afa9dacbd5cf676cd6d60bca
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-30 10:15:03 +0300

    Create part-z-0008-while1.dart

---
+++ part-z-0008-while1.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  var counter = 0;
+
+  while (counter < 5) {
+    print("Number${counter + 1}: $counter");
+
+    counter++;
+  }
+}
+
+// Number1: 0
+// Number2: 1
+// Number3: 2
+// Number4: 3
+// Number5: 4
