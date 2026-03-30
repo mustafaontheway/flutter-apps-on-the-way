68431376827445e5550339ceb6925fea36b680cd
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-30 09:08:21 +0300

    Create part-z-0002-bool-type1.dart

---
+++ part-z-0002-bool-type1.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  var isTrue = true;
+
+  bool completed = false;
+
+  var result1 = 3 > 2 && 4 != 5;
+
+  print(result1);
+
+  var result2 = false || 3 == 3.01;
+
+  print(result2);
+}
+
+// true
+// false
