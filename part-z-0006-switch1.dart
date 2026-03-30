a91f306132f5d06eba4572ae042d8dabbe2db044
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-03-30 09:47:03 +0300

    Create part-z-0006-switch1.dart

---
+++ part-z-0006-switch1.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  var month = 3;
+
+  switch (month) {
+    case 12 || 1 || 2:
+      print("Winter");
+    case 3 || 4 || 5:
+      print("Spring");
+    case 6 || 7 || 8:
+      print("Summer");
+    case 9 || 10 || 11:
+      print("Autumn");
+    default:
+      print("Type an intger between 1 and 12");
+  }
+}
+
+// x > y
