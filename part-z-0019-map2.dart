fbf570317ff3b0efa1db47ee27ee35802a155315
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-01 20:51:31 +0300

    Create part-z-0019-map2.dart

---
+++ part-z-0019-map2.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  Map<String, int> salaryByID = {"au004296": 75000, "kt007897": 92000};
+
+  salaryByID["zy006587"] = 54200;
+
+  salaryByID["au004296"] = 78000;
+
+  print(salaryByID);
+}
+
+// {au004296: 78000, kt007897: 92000, zy006587: 54200}
