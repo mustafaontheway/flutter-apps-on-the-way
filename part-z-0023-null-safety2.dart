f48fd679698818b3a8aff0765d95eadaeae3cc20
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-19 10:07:47 +0300

    Create part-z-0023-null-safety2.dart

---
+++ part-z-0023-null-safety2.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  String? name = null;
+  
+  if (name != null) {
+    
+     print("Your name is ${name}.");
+  }
+  
+  else {
+    
+    print("Type your name please!");
+  }
+}
+
+
+// Type your name please!
