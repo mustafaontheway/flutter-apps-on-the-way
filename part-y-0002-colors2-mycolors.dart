c34aaa4b3627530321910de17968aada44f3a0e9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-20 22:51:01 +0300

    Create part-y-0002-colors2-mycolors.dart

---
+++ part-y-0002-colors2-mycolors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+var textColor = const Color(0xFF0C2340);
+
+var buttonColor = const Color(0xFF153D6E);
+
+var appBarColor = const Color(0xFF2C81E8);
+
+
+
+// https://color.adobe.com/explore?q=finance&color-palette=2C81E8%2C246ABF%2C0C2340%2C1D5396%2C153D6E&color-palette-name=My+Color+Theme
