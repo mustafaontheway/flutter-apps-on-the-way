063b41db20e3ea100a6bda30bf0ce53833cd438a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-20 23:40:03 +0300

    Create part-y-0003-fonts3-mymainpage.dart

---
+++ part-y-0003-fonts3-mymainpage.dart
@@ -0,0 +1,28 @@
+import 'package:first/mycolors.dart';
+import 'package:flutter/material.dart';
+
+class Mymainpage extends StatefulWidget {
+  const Mymainpage({super.key});
+
+  @override
+  State<Mymainpage> createState() => _MymainpageState();
+}
+
+class _MymainpageState extends State<Mymainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome Fintech User", style: TextStyle(color: Colors.white, fontFamily: "Mozilla", fontSize: 25),), backgroundColor: appBarColor, centerTitle: true,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("FinTech Innovation will be here!", style: TextStyle(fontSize: 25, color:textColor ),),
+            const SizedBox(height: 15,),
+            Text("Let's do it!", style: TextStyle(fontSize: 20, color: textColor),)
+          ],
+        ),
+      ),
+    );
+  }
+}
